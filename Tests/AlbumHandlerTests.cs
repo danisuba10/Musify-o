7112eaa6b8d62807d7e402148f9d77f0e32317ff
Suba Dániel <danisuba10@gmail.com>
2024-08-12 23:56:35 +0300

    Add GetAlbumById and GetAllAlbums tests

--- Tests/AlbumHandlerTests.cs
+++ Tests/AlbumHandlerTests.cs
@@ -44,5 +44,51 @@ namespace Tests
             Assert.NotNull(returnAlbum);
             Assert.Equal(album.Name, returnAlbum.Name);
         }
+
+        [Fact]
+        public async Task GetAlbumByID_ShouldReturnAlbum()
+        {
+            //Arrange
+            var album = new Album { Name = "Follow the leader", ImageLocation = "path/test" };
+            _context.Albums.Add(album);
+            await _context.SaveChangesAsync();
+
+            var handler = new GetAlbumByID.Handler(_context);
+            var query = new GetAlbumByID.Query { Id = album.Id };
+
+            //Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(album.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task GetAllAlbums_ShouldReturnAllAlbums()
+        {
+            //Arrange
+            var albums = _context.Albums;
+            _context.Albums.RemoveRange(albums);
+            await _context.SaveChangesAsync();
+            _context.Albums.AddRange(new List<Album>
+            {
+                new Album {Name = "Follow the leader", ImageLocation = "path/1"},
+                new Album {Name = "Surrealistic pillow", ImageLocation = "path/2"},
+                new Album {Name = "Reggatta De Blanc", ImageLocation = "path/3"},
+                new Album {Name = "Rosenrot", ImageLocation = "path/4"}
+            }
+            );
+            await _context.SaveChangesAsync();
+
+            var handler = new GetAllAlbums.Handler(_context);
+            var query = new GetAllAlbums.Query();
+
+            //Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            Assert.Equal(4, result.Count);
+        }
     }
 }
