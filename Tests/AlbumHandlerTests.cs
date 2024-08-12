f603a8362b378284eba5df74f5d496a430f38f25
Suba Dániel <danisuba10@gmail.com>
2024-08-12 23:20:54 +0300

    Create unit tests

---
+++ Tests/AlbumHandlerTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using MediatR;
+using Persistence;
+using Domain;
+using Application.Albums;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Tests
+{
+    public class AlbumHandlerTests
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMediator _mediator;
+
+        public AlbumHandlerTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+
+            var mediatorMock = new Mock<IMediator>();
+            _mediator = mediatorMock.Object;
+        }
+
+        [Fact]
+        public async Task AddAlbum_ShouldAddAlbumToDatabase()
+        {
+            var handler = new AddAlbum.Handler(_context);
+            var album = new Album { Name = "Follow the leader", ImageLocation = "path/followtheleader" };
+            var command = new AddAlbum.Command { Album = album };
+
+            //Act
+            await handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            var returnAlbum = await _context.Albums.FirstOrDefaultAsync(a => a.Name == "Follow the leader");
+            Assert.NotNull(returnAlbum);
+            Assert.Equal(album.Name, returnAlbum.Name);
+        }
+    }
+}
