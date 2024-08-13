0a2723dc6d002873bcdee68c4d2558c0402a867e
Suba Dániel <danisuba10@gmail.com>
2024-08-13 19:10:56 +0300

    Added test for Album delete behaviour, making sure Songs are deleted when Album is deleted

--- Tests/AlbumHandlerTests.cs
+++ Tests/AlbumHandlerTests.cs
@@ -9,6 +9,7 @@ using Domain;
 using Application.Albums;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using Application;
 
 namespace Tests
 {
@@ -92,6 +93,51 @@ namespace Tests
         }
 
         [Fact]
-        public async Task Remove
+        public async Task RemoveAlbumByID_ShouldRemoveAlbumAndRelatedSongs()
+        {
+            //Arrange
+            var albums = _context.Albums;
+            _context.Albums.RemoveRange(albums);
+            await _context.SaveChangesAsync();
+            List<Album> NewAlbums = new List<Album>
+            {
+                new Album {Name = "Follow the leader", ImageLocation = "path/1"},
+                new Album {Name = "Surrealistic pillow", ImageLocation = "path/2"},
+                new Album {Name = "Reggatta De Blanc", ImageLocation = "path/3"},
+                new Album {Name = "Rosenrot", ImageLocation = "path/4"}
+            };
+            _context.Albums.AddRange(NewAlbums);
+            List<Song> NewSongs = new List<Song>
+            {
+                new Song{Title = "Freak On a Leash", Duration = TimeSpan.FromSeconds(256), AlbumId = NewAlbums[0].Id},
+                new Song{Title = "Got The Life", Duration = TimeSpan.FromSeconds(248), AlbumId = NewAlbums[0].Id},
+                new Song{Title = "White Rabbit", Duration = TimeSpan.FromSeconds(151), AlbumId = NewAlbums[1].Id},
+                new Song{Title = "Message In A Bottle", Duration = TimeSpan.FromSeconds(290), AlbumId = NewAlbums[2].Id},
+                new Song{Title = "Rosenrot", Duration = TimeSpan.FromSeconds(235), AlbumId = NewAlbums[3].Id}
+            };
+            _context.Songs.AddRange(NewSongs);
+            await _context.SaveChangesAsync();
+
+            var initialAlbumCount = await _context.Albums.CountAsync();
+            Assert.Equal(4, initialAlbumCount);
+
+            var handler = new RemoveAlbumByID.Handler(_context);
+            var command = new RemoveAlbumByID.Command { Id = NewAlbums[0].Id };
+
+            //Act
+            try
+            {
+                await handler.Handle(command, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            var AlbumsAfterDelete = _context.Albums;
+            Assert.Equal(3, AlbumsAfterDelete.Count());
+            var SongsAfterDelete = _context.Songs;
+            Assert.Equal(3, SongsAfterDelete.Count());
+        }
     }
 }
