679ef63eb2f408e9b35a9d0b3e4de6eff0455642
Suba Dániel <danisuba10@gmail.com>
2024-08-14 12:43:41 +0300

    UpdateAlbumByID

---
+++ Application/Albums/UpdateAlbumByID.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Albums
+{
+    public class UpdateAlbumByID
+    {
+        public class Query : IRequest
+        {
+            public int Id { get; set; }
+            public Album Album { get; set; } = new Album();
+        }
+
+        public class Handler : IRequestHandler<Query>
+        {
+            private readonly ApplicationDbContext _context;
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Query query, CancellationToken cancellationToken)
+            {
+                var existingAlbum = await _context.Albums
+                    .FirstOrDefaultAsync(a => a.Id == query.Id, cancellationToken);
+
+                if (existingAlbum == null)
+                {
+                    throw new Exception("Album does not exist!");
+                }
+
+                Album newAlbum = query.Album;
+                if (newAlbum.Name != "")
+                {
+                    existingAlbum.Name = newAlbum.Name;
+                }
+
+                if (newAlbum.ImageLocation != null)
+                {
+                    existingAlbum.ImageLocation = newAlbum.ImageLocation;
+                }
+
+                if (newAlbum.Songs != null && newAlbum.Songs.Any())
+                {
+                    existingAlbum.Songs = newAlbum.Songs;
+                }
+
+                _context.Albums.Update(existingAlbum);
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
