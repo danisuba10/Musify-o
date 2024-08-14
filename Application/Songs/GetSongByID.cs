fd8a276e7b3995e8cb3ff85396e68c5faf4a4b39
Suba Dániel <danisuba10@gmail.com>
2024-08-14 14:14:39 +0300

    GetSongByID

---
+++ Application/Songs/GetSongByID.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Songs
+{
+    public class GetSongByID
+    {
+        public class Query : IRequest<Song>
+        {
+            public int Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Song>
+        {
+            private readonly ApplicationDbContext _context;
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Song> Handle(Query query, CancellationToken cancellationToken)
+            {
+                var song = await _context.Songs
+                    .Include(s => s.Album)
+                    .FirstOrDefaultAsync(s => s.Id == query.Id, cancellationToken);
+
+                if (song == null)
+                {
+                    throw new Exception($"Song with ID {query.Id} does not exists.");
+                }
+
+                return song;
+            }
+
+        }
+    }
+}
