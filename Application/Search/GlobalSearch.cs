135a643bb26c9b29d6f6e31b667daef4fa8f0d42
Suba Dániel <danisuba10@gmail.com>
2024-08-15 18:44:52 +0300

    Implement Global Search which retrieves IDs of Album, Song or Artist objects

---
+++ Application/Search/GlobalSearch.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Search
+{
+    public class GlobalSearch
+    {
+        public class Query : IRequest<List<SearchResult>>
+        {
+            public required string SearchString { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, List<SearchResult>>
+        {
+            private readonly ApplicationDbContext _context;
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<SearchResult>> Handle(Query query, CancellationToken cancellationToken)
+            {
+                var songs = await _context.Songs
+                    .Where(s => s.Title.Contains(query.SearchString, StringComparison.OrdinalIgnoreCase))
+                    .Select(s => new SearchResult { Type = "Song", Id = s.Id })
+                    .ToListAsync(cancellationToken);
+
+                var albums = await _context.Albums
+                    .Where(a => a.Name.Contains(query.SearchString, StringComparison.OrdinalIgnoreCase))
+                    .Select(a => new SearchResult { Type = "Album", Id = a.Id })
+                    .ToListAsync(cancellationToken);
+
+                var artists = await _context.Artists
+                    .Where(art => art.Name.Contains(query.SearchString, StringComparison.OrdinalIgnoreCase))
+                    .Select(art => new SearchResult { Type = "Artist", Id = art.Id })
+                    .ToListAsync(cancellationToken);
+
+                var result = songs.Concat(albums).Concat(artists).ToList();
+
+                return result;
+            }
+        }
+
+    }
+}
---
+++ Application/Search/SearchResult.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Search
+{
+    public class SearchResult
+    {
+        public required string Type { get; set; }
+        public required int Id { get; set; }
+    }
+}
