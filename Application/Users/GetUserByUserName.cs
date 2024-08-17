6c974ab3de12f71fdd4266831a569168ecc2f97d
Suba Dániel <danisuba10@gmail.com>
2024-08-17 22:28:42 +0300

    Change GetUserByUserName to allow it to return NULL

--- Application/Users/GetUserByUserName.cs
+++ Application/Users/GetUserByUserName.cs
@@ -17,21 +17,21 @@ namespace Application.Users
             public required string UserName { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, User>
+        public class Handler : IRequestHandler<Query, User?>
         {
             private readonly ApplicationDbContext _context;
             public Handler(ApplicationDbContext context)
             {
                 _context = context;
             }
-            public async Task<User> Handle(Query query, CancellationToken cancellationToken)
+            public async Task<User?> Handle(Query query, CancellationToken cancellationToken)
             {
                 var user = await _context.Users
                     .FirstOrDefaultAsync(u => u.UserName == query.UserName, cancellationToken);
 
                 if (user == null)
                 {
-                    throw new Exception("Username does not exist!");
+                    return null;
                 }
 
                 return user;
