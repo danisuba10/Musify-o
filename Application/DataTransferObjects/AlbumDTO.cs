ffc6d2663d59194f58ba4d505568a2e6b9424865
Suba Dániel <danisuba10@gmail.com>
2024-08-23 19:49:36 +0300

    Make Songs and Artists nullable

--- Application/DataTransferObjects/AlbumDTO.cs
+++ Application/DataTransferObjects/AlbumDTO.cs
@@ -10,7 +10,7 @@ namespace Application.DataTransferObjects
         public string AlbumName { get; set; } = string.Empty;
         public string? ImageLocation { get; set; } = null;
         public Guid? Id { get; set; }
-        public List<SongDTO> Songs { get; set; } = new List<SongDTO>();
-        public List<ArtistDTO> Artists { get; set; } = new List<ArtistDTO>();
+        public List<SongDTO>? Songs { get; set; } = new List<SongDTO>();
+        public List<ArtistDTO>? Artists { get; set; } = new List<ArtistDTO>();
     }
 }
