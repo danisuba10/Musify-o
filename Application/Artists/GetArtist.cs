1163ff25d86705ef995f2525479dda962ffc18cc
Suba Dániel <danisuba10@gmail.com>
2024-08-23 13:37:03 +0300

    Create GetArtist and delete GetArtistByName

---
+++ Application/Artists/GetArtist.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Artists
+{
+    public class GetArtist
+    {
+        public class Query : IRequest<Artist?>
+        {
+            public Guid? Id { get; set; } = null;
+            public string? Name { get; set; } = null;
+        }
+        public class Handler : IRequestHandler<Query, Artist?>
+        {
+            ApplicationDbContext _context;
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<Artist?> Handle(Query query, CancellationToken cancellationToken)
+            {
+                if (query.Id == null && query.Name == null)
+                {
+                    return null;
+                }
+
+                IQueryable<Artist> artistsQuery = _context.Artists;
+
+                if (!String.IsNullOrWhiteSpace(query.Name))
+                {
+                    artistsQuery.Where(a => a.Name == query.Name);
+                }
+
+                if (query.Id != null)
+                {
+                    artistsQuery.Where(a => a.Id == query.Id);
+                }
+
+                var Artist = await artistsQuery.
+                    FirstOrDefaultAsync(cancellationToken);
+
+                return Artist;
+            }
+        }
+    }
+}
