2a64c0d0f2b847d1074753827205db94ef3d6ec1
Suba Dániel <danisuba10@gmail.com>
2024-10-02 09:25:07 +0300

    Modify SongMapper to new SongResponse structure having ArtistResponses and ArtistIds

--- Application/Mappers/SongMapper.cs
+++ Application/Mappers/SongMapper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using Application.DataTransferObjects.Responses;
 using Domain;
 
@@ -11,7 +12,7 @@ namespace Application.Mappers
     {
         public static SongResponse MapToResponse(Song song)
         {
-            return new SongResponse
+            SongResponse response = new SongResponse
             {
                 Id = song.Id,
                 Title = song.Title,
@@ -19,8 +20,17 @@ namespace Application.Mappers
                 AlbumId = song.AlbumId,
                 Album = song.Album,
                 PositionInAlbum = song.PositionInAlbum,
-                Artists = song.SongArtistRelations.Select(ar => ar.Artist).ToList()
             };
+
+            List<Artist> artists = song.SongArtistRelations.Select(ar => ar.Artist).ToList();
+            response.Artists = ArtistMapper.MapToResponseList(artists);
+
+            foreach (Artist artist in artists)
+            {
+                response.ArtistIds.Add(artist.Id);
+            }
+
+            return response;
         }
 
         public static List<SongResponse> MapToResponseList(List<Song> songs)
