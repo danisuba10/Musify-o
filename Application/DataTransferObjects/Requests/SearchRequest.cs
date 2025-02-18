d601482c0414d1652a8e7e0ca74bb979ab528543
Suba Dániel <danisuba10@gmail.com>
2025-02-18 20:24:02 +0200

    Allow for search term to be null or empty

--- Application/DataTransferObjects/Requests/SearchRequest.cs
+++ Application/DataTransferObjects/Requests/SearchRequest.cs
@@ -7,7 +7,7 @@ namespace Application.DataTransferObjects.Requests
 {
     public class SearchRequest
     {
-        public required string SearchTerm { get; set; }
+        public string? SearchTerm { get; set; } = "";
         public string? LastName { get; set; }
         public DateTime? LastCreatedAt { get; set; }
         public int PageSize { get; set; }
