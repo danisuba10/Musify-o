179542c5aa8d9469146e1d390b6e945ac4833206
Suba Dániel <danisuba10@gmail.com>
2025-02-22 23:11:12 +0200

    Rename ClaimType for user identifier in JWT token generation

--- Application/Services/JwtTokenService.cs
+++ Application/Services/JwtTokenService.cs
@@ -33,7 +33,7 @@ namespace Application.Services
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim("Identifier", user.Id.ToString()),
                 new Claim("role", user.Role)
             };
 
