f47a1bb93bf075962b7122d027078001424d302f
Suba Dániel <danisuba10@gmail.com>
2025-02-23 23:01:18 +0200

    Migration: Change PlayItemType of PlayRecords to string

---
+++ Persistence/Migrations/20250223210000_ChangePlayRecordTypeToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePlayRecordTypeToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PlayedItemType",
+                table: "PlayRecords",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PlayedItemType",
+                table: "PlayRecords",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
