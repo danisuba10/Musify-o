092bbc5b80d4ca21942a8f02713e0d38cf88cb4e
Suba Dániel <danisuba10@gmail.com>
2025-03-01 23:28:25 +0200

    Make Visibility enum appear as string in Swagger

--- API/Program.cs
+++ API/Program.cs
@@ -62,6 +62,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
     {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Musify API", Version = "v1" });
+        c.UseInlineDefinitionsForEnums();
         c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
             Name = "Authorization",
