e0dc6228f7d6650b0a9669ff70cc0ca421ffa4ec
Suba Dániel <danisuba10@gmail.com>
2025-03-06 13:57:39 +0200

    Rename UserResponse to UserResponseCompact in PlaylistResponse DTO

--- Application/DataTransferObjects/Responses/PlaylistResponse.cs
+++ Application/DataTransferObjects/Responses/PlaylistResponse.cs
@@ -9,7 +9,7 @@ namespace Application.DataTransferObjects.Responses
     {
         public required Guid Id { get; set; }
         public required Guid UserId { get; set; }
-        public UserResponse? User { get; set; }
+        public UserResponseCompact? User { get; set; }
         public required string Name { get; set; }
         public ImageResponse? Image { get; set; } = null;
         public required List<Guid> SongIds { get; set; }
