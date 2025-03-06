780eb0e3d72c05faafde9607eba53dbeae4d2e1d
Suba Dániel <danisuba10@gmail.com>
2025-03-06 13:37:22 +0200

    Add PlaylistResponse and UserResponse DTOs for playlist and user data handling

---
+++ Application/DataTransferObjects/Responses/PlaylistResponse.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.DataTransferObjects.Responses
+{
+    public class PlaylistResponse
+    {
+        public required Guid Id { get; set; }
+        public required Guid UserId { get; set; }
+        public UserResponse? User { get; set; }
+        public required string Name { get; set; }
+        public ImageResponse? Image { get; set; } = null;
+        public required List<Guid> SongIds { get; set; }
+        public List<SongResponse>? Songs { get; set; } = null;
+        public required int Duration { get; set; }
+        public required int SongCount { get; set; }
+    }
+}
