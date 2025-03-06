36559c3ac6b5c4b2b4912393364c62b7f537fd16
Suba Dániel <danisuba10@gmail.com>
2025-03-06 14:04:54 +0200

    Do update migration on startup, so when we move it to the VPS it can auto update changes from Git

--- API/Program.cs
+++ API/Program.cs
@@ -176,6 +176,21 @@ builder.Services.AddAuthorization(options =>
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+
 app.UseCors("AllowLocalHost");
 
 
