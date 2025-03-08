29ce5e5233fcb7b2009ada2f90d12c4c43eca397
Suba Dániel <danisuba10@gmail.com>
2025-03-08 13:14:51 +0200

    Add Description and Visibility properties to PlaylistResponse

--- Application/DataTransferObjects/Responses/PlaylistResponse.cs
+++ Application/DataTransferObjects/Responses/PlaylistResponse.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Application.DataTransferObjects.Responses
 {
@@ -9,6 +10,7 @@ namespace Application.DataTransferObjects.Responses
     {
         public required Guid Id { get; set; }
         public required Guid UserId { get; set; }
+        public required string Description { get; set; }
         public UserResponseCompact? User { get; set; }
         public required string Name { get; set; }
         public ImageResponse? Image { get; set; } = null;
@@ -16,5 +18,6 @@ namespace Application.DataTransferObjects.Responses
         public List<SongResponse>? Songs { get; set; } = null;
         public required int Duration { get; set; }
         public required int SongCount { get; set; }
+        public required Visibility Visibility { get; set; }
     }
 }
