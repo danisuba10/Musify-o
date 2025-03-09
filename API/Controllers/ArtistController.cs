e5f041664440eb67a62a5d2b4c9bffac917f3c8d
Suba Dániel <danisuba10@gmail.com>
2025-03-09 20:04:57 +0200

    Secure ArtistController for public use

--- API/Controllers/ArtistController.cs
+++ API/Controllers/ArtistController.cs
@@ -40,80 +40,80 @@ namespace API.Controllers
             return Path.Combine("artist", name + ".jpg");
         }
 
-        [HttpPost("GetArtistByName")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ArtistDTO))]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetArtistByName(string Name, CancellationToken cancellationToken)
-        {
-            var artist = await Mediator.Send(new GetArtist.Query { Name = Name });
+        // [HttpPost("GetArtistByName")]
+        // [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ArtistDTO))]
+        // [ProducesResponseType(StatusCodes.Status404NotFound)]
+        // public async Task<IActionResult> GetArtistByName(string Name, CancellationToken cancellationToken)
+        // {
+        //     var artist = await Mediator.Send(new GetArtist.Query { Name = Name });
 
-            if (artist == null)
-            {
-                return NotFound(new { Message = "Artist not found" });
-            }
+        //     if (artist == null)
+        //     {
+        //         return NotFound(new { Message = "Artist not found" });
+        //     }
 
-            return Ok(new ArtistDTO { Name = artist.Name, ImageLocation = artist.ImageLocation, Id = artist.Id });
-        }
+        //     return Ok(new ArtistDTO { Name = artist.Name, ImageLocation = artist.ImageLocation, Id = artist.Id });
+        // }
 
-        [HttpGet("GetArtistID")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> GetArtistID(string name, CancellationToken cancellationToken)
-        {
-            var artist = await Mediator.Send(new GetArtist.Query { Name = name }, cancellationToken);
-            if (artist != null)
-            {
-                return Ok(artist.Id);
-            }
-            else
-            {
-                return BadRequest("Artist does not exist!");
-            }
-        }
+        // [HttpGet("GetArtistID")]
+        // [ProducesResponseType(StatusCodes.Status200OK)]
+        // [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        // public async Task<IActionResult> GetArtistID(string name, CancellationToken cancellationToken)
+        // {
+        //     var artist = await Mediator.Send(new GetArtist.Query { Name = name }, cancellationToken);
+        //     if (artist != null)
+        //     {
+        //         return Ok(artist.Id);
+        //     }
+        //     else
+        //     {
+        //         return BadRequest("Artist does not exist!");
+        //     }
+        // }
 
-        [HttpGet("GetAllArtistIds")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Guid>))]
-        public async Task<List<Guid>> GetAllArtistIDs()
-        {
-            List<Guid> Ids = new List<Guid>();
-            var artists = await Mediator.Send(new GetAllArtists.Query { });
+        // [HttpGet("GetAllArtistIds")]
+        // [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Guid>))]
+        // public async Task<List<Guid>> GetAllArtistIDs()
+        // {
+        //     List<Guid> Ids = new List<Guid>();
+        //     var artists = await Mediator.Send(new GetAllArtists.Query { });
 
-            foreach (var artist in artists)
-            {
-                Ids.Add(artist.Id);
-            }
+        //     foreach (var artist in artists)
+        //     {
+        //         Ids.Add(artist.Id);
+        //     }
 
-            return Ids;
-        }
+        //     return Ids;
+        // }
 
-        [HttpPost("AddArtistImage")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> addArtistImage(Guid ArtistID, IFormFile formFile, CancellationToken cancellationToken)
-        {
-            var album = await Mediator.Send(new GetArtist.Query { Id = ArtistID });
-            if (album != null)
-            {
-                try
-                {
-                    await Mediator.Send(new UploadImage.Command
-                    {
-                        formFile = formFile,
-                        Path = Path.Combine(ImageFolderPath, "Artists1"),
-                        Name = ArtistID.ToString()
-                    });
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-                return Ok();
-            }
-            else
-            {
-                return BadRequest("Album does not exist!");
-            }
-        }
+        // [HttpPost("AddArtistImage")]
+        // [ProducesResponseType(StatusCodes.Status200OK)]
+        // [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        // public async Task<IActionResult> addArtistImage(Guid ArtistID, IFormFile formFile, CancellationToken cancellationToken)
+        // {
+        //     var album = await Mediator.Send(new GetArtist.Query { Id = ArtistID });
+        //     if (album != null)
+        //     {
+        //         try
+        //         {
+        //             await Mediator.Send(new UploadImage.Command
+        //             {
+        //                 formFile = formFile,
+        //                 Path = Path.Combine(ImageFolderPath, "Artists1"),
+        //                 Name = ArtistID.ToString()
+        //             });
+        //         }
+        //         catch (Exception ex)
+        //         {
+        //             return BadRequest(ex.Message);
+        //         }
+        //         return Ok();
+        //     }
+        //     else
+        //     {
+        //         return BadRequest("Album does not exist!");
+        //     }
+        // }
 
         [Authorize(Policy = "Admin")]
         [HttpPost("add-artist")]
