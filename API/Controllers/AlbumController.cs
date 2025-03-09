0b1c1c96b2cfa21a18535372f3730f02c4db2809
Suba Dániel <danisuba10@gmail.com>
2025-03-09 20:03:31 +0200

    Secure AlbumController for public avaibility

--- API/Controllers/AlbumController.cs
+++ API/Controllers/AlbumController.cs
@@ -42,170 +42,170 @@ namespace API.Controllers
             return Path.Combine("album", name + ".jpg");
         }
 
-        private async Task<Artist> GetOrCreateArtist
-        (ArtistDTO artistDTO, HashSet<Artist> usedArtists, CancellationToken cancellationToken)
-        {
-            var Artist = usedArtists.FirstOrDefault(a => a.Name == artistDTO.Name);
+        // private async Task<Artist> GetOrCreateArtist
+        // (ArtistDTO artistDTO, HashSet<Artist> usedArtists, CancellationToken cancellationToken)
+        // {
+        //     var Artist = usedArtists.FirstOrDefault(a => a.Name == artistDTO.Name);
 
-            if (Artist == null)
-            {
-                Artist = await Mediator.Send(new GetArtist.Query { Name = artistDTO.Name });
+        //     if (Artist == null)
+        //     {
+        //         Artist = await Mediator.Send(new GetArtist.Query { Name = artistDTO.Name });
 
-                if (Artist == null)
-                {
-                    Guid ArtistID;
+        //         if (Artist == null)
+        //         {
+        //             Guid ArtistID;
 
-                    if (artistDTO.Id != null)
-                    {
-                        ArtistID = (Guid)artistDTO.Id;
-                    }
-                    else
-                    {
-                        ArtistID = Guid.NewGuid();
-                    }
+        //             if (artistDTO.Id != null)
+        //             {
+        //                 ArtistID = (Guid)artistDTO.Id;
+        //             }
+        //             else
+        //             {
+        //                 ArtistID = Guid.NewGuid();
+        //             }
 
-                    Artist = new Artist()
-                    {
-                        Id = ArtistID,
-                        Name = artistDTO.Name
-                    };
-                    Artist.ImageLocation = "artists/" + Artist.Id;
-                }
+        //             Artist = new Artist()
+        //             {
+        //                 Id = ArtistID,
+        //                 Name = artistDTO.Name
+        //             };
+        //             Artist.ImageLocation = "artists/" + Artist.Id;
+        //         }
 
-                usedArtists.Add(Artist);
-            }
-            return Artist;
-        }
+        //         usedArtists.Add(Artist);
+        //     }
+        //     return Artist;
+        // }
 
-        [HttpPost("AddAlbumWithComplexData")]
-        public async Task<IActionResult> AddAlbumWithComplexData
-        ([FromBody] AlbumDTO albumDto, CancellationToken cancellationToken)
-        {
-            HashSet<Artist> UsedArtists = new HashSet<Artist>();
+        // [HttpPost("AddAlbumWithComplexData")]
+        // public async Task<IActionResult> AddAlbumWithComplexData
+        // ([FromBody] AlbumDTO albumDto, CancellationToken cancellationToken)
+        // {
+        //     HashSet<Artist> UsedArtists = new HashSet<Artist>();
 
-            Guid AlbumID;
-            if (albumDto.Id != null)
-            {
-                AlbumID = (Guid)albumDto.Id;
-            }
-            else
-            {
-                AlbumID = Guid.NewGuid();
-            }
-            var Album = new Album
-            {
-                Id = AlbumID,
-                Name = albumDto.Name,
-                Songs = new HashSet<Song>(),
-                AlbumArtistRelations = new List<AlbumArtistRelation>()
-            };
-            Album.ImageLocation = "albums/" + Album.Id;
+        //     Guid AlbumID;
+        //     if (albumDto.Id != null)
+        //     {
+        //         AlbumID = (Guid)albumDto.Id;
+        //     }
+        //     else
+        //     {
+        //         AlbumID = Guid.NewGuid();
+        //     }
+        //     var Album = new Album
+        //     {
+        //         Id = AlbumID,
+        //         Name = albumDto.Name,
+        //         Songs = new HashSet<Song>(),
+        //         AlbumArtistRelations = new List<AlbumArtistRelation>()
+        //     };
+        //     Album.ImageLocation = "albums/" + Album.Id;
 
-            List<String> ArtistsString = new List<String>();
-            foreach (var ArtistDTO in albumDto.Artists)
-            {
+        //     List<String> ArtistsString = new List<String>();
+        //     foreach (var ArtistDTO in albumDto.Artists)
+        //     {
 
-                var Artist = await GetOrCreateArtist(ArtistDTO, UsedArtists, cancellationToken);
+        //         var Artist = await GetOrCreateArtist(ArtistDTO, UsedArtists, cancellationToken);
 
-                Album.AlbumArtistRelations.Add(
-                    new AlbumArtistRelation
-                    {
-                        Album = Album,
-                        Artist = Artist
-                    }
-                    );
-                ArtistsString.Add(Artist.Name);
-            }
+        //         Album.AlbumArtistRelations.Add(
+        //             new AlbumArtistRelation
+        //             {
+        //                 Album = Album,
+        //                 Artist = Artist
+        //             }
+        //             );
+        //         ArtistsString.Add(Artist.Name);
+        //     }
 
-            var ExistingAlbum = await Mediator.Send(
-                new SearchAlbums.Query { Name = Album.Name, Artists = ArtistsString, AllArtistsPresent = true });
+        //     var ExistingAlbum = await Mediator.Send(
+        //         new SearchAlbums.Query { Name = Album.Name, Artists = ArtistsString, AllArtistsPresent = true });
 
-            if (ExistingAlbum != null)
-            {
-                return BadRequest("Album is already added!");
-            }
+        //     if (ExistingAlbum != null)
+        //     {
+        //         return BadRequest("Album is already added!");
+        //     }
 
-            foreach (var SongDTO in albumDto.Songs)
-            {
-                Guid SongID;
-                if (SongDTO.Id != null)
-                {
-                    SongID = (Guid)SongDTO.Id;
-                }
-                else
-                {
-                    SongID = Guid.NewGuid();
-                }
-                var Song = new Song()
-                {
-                    Id = SongID,
-                    Title = SongDTO.Title,
-                    Duration = SongDTO.Duration,
-                    PositionInAlbum = SongDTO.PositionInAlbum,
-                    Album = Album,
-                    SongArtistRelations = new List<SongArtistRelation>()
-                };
+        //     foreach (var SongDTO in albumDto.Songs)
+        //     {
+        //         Guid SongID;
+        //         if (SongDTO.Id != null)
+        //         {
+        //             SongID = (Guid)SongDTO.Id;
+        //         }
+        //         else
+        //         {
+        //             SongID = Guid.NewGuid();
+        //         }
+        //         var Song = new Song()
+        //         {
+        //             Id = SongID,
+        //             Title = SongDTO.Title,
+        //             Duration = SongDTO.Duration,
+        //             PositionInAlbum = SongDTO.PositionInAlbum,
+        //             Album = Album,
+        //             SongArtistRelations = new List<SongArtistRelation>()
+        //         };
 
-                foreach (var ArtistDTO in SongDTO.Artists)
-                {
-                    var Artist = await GetOrCreateArtist(ArtistDTO, UsedArtists, cancellationToken);
+        //         foreach (var ArtistDTO in SongDTO.Artists)
+        //         {
+        //             var Artist = await GetOrCreateArtist(ArtistDTO, UsedArtists, cancellationToken);
 
-                    Song.SongArtistRelations.Add(new SongArtistRelation
-                    {
-                        Song = Song,
-                        Artist = Artist
-                    }
-                    );
-                }
+        //             Song.SongArtistRelations.Add(new SongArtistRelation
+        //             {
+        //                 Song = Song,
+        //                 Artist = Artist
+        //             }
+        //             );
+        //         }
 
-                Album.Songs.Add(Song);
-            }
+        //         Album.Songs.Add(Song);
+        //     }
 
-            await Mediator.Send(new AddAlbum.Command { Album = Album });
+        //     await Mediator.Send(new AddAlbum.Command { Album = Album });
 
-            return Ok(Album);
-        }
+        //     return Ok(Album);
+        // }
 
-        [HttpGet("GetAlbumID")]
-        public async Task<IActionResult> GetAlbumId(string name, CancellationToken cancellationToken)
-        {
-            var albums = await Mediator.Send(new SearchAlbums.Query { Name = name });
-            if (albums != null)
-            {
-                return Ok(albums[0].Id);
-            }
-            else
-            {
-                return BadRequest("Album does not exist!");
-            }
-        }
+        // [HttpGet("GetAlbumID")]
+        // public async Task<IActionResult> GetAlbumId(string name, CancellationToken cancellationToken)
+        // {
+        //     var albums = await Mediator.Send(new SearchAlbums.Query { Name = name });
+        //     if (albums != null)
+        //     {
+        //         return Ok(albums[0].Id);
+        //     }
+        //     else
+        //     {
+        //         return BadRequest("Album does not exist!");
+        //     }
+        // }
 
-        [HttpPost("AddAlbumImage")]
-        public async Task<IActionResult> AddAlbumImage(Guid AlbumID, IFormFile formFile, CancellationToken cancellationToken)
-        {
-            var album = await Mediator.Send(new GetAlbumByID.Query { Id = AlbumID });
-            if (album != null)
-            {
-                try
-                {
-                    await Mediator.Send(new UploadImage.Command
-                    {
-                        formFile = formFile,
-                        Path = Path.Combine(ImageFolderPath, "Albums"),
-                        Name = AlbumID.ToString()
-                    });
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-                return Ok();
-            }
-            else
-            {
-                return BadRequest("Album does not exist!");
-            }
-        }
+        // [HttpPost("AddAlbumImage")]
+        // public async Task<IActionResult> AddAlbumImage(Guid AlbumID, IFormFile formFile, CancellationToken cancellationToken)
+        // {
+        //     var album = await Mediator.Send(new GetAlbumByID.Query { Id = AlbumID });
+        //     if (album != null)
+        //     {
+        //         try
+        //         {
+        //             await Mediator.Send(new UploadImage.Command
+        //             {
+        //                 formFile = formFile,
+        //                 Path = Path.Combine(ImageFolderPath, "Albums"),
+        //                 Name = AlbumID.ToString()
+        //             });
+        //         }
+        //         catch (Exception ex)
+        //         {
+        //             return BadRequest(ex.Message);
+        //         }
+        //         return Ok();
+        //     }
+        //     else
+        //     {
+        //         return BadRequest("Album does not exist!");
+        //     }
+        // }
 
         // [HttpPost("AddSongToAlbum")]
         // public async Task<IActionResult> AddSongToAlbum(Guid AlbumID, Guid SongID)
