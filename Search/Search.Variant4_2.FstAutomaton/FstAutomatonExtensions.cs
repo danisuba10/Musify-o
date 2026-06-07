255be5848313d0c4e19d681d8dd5f91c3bc3c5f8
Suba Daniel <danisuba10@gmail.com>
2026-06-07 17:05:40 +0300

    fix cache bugs search

--- Search/Search.Variant4_2.FstAutomaton/FstAutomatonExtensions.cs
+++ Search/Search.Variant4_2.FstAutomaton/FstAutomatonExtensions.cs
@@ -6,7 +6,7 @@ namespace Search.Variant4_2.FstAutomaton;
 
 public static class FstAutomatonExtensions
 {
-    public static IServiceCollection AddVariant4_Search(this IServiceCollection services)
+    public static IServiceCollection AddVariant4_2_Search(this IServiceCollection services)
     {
         services.AddMemoryCache(opts => opts.SizeLimit = 10_000);
         services.AddSingleton<FstTermIndex>();
